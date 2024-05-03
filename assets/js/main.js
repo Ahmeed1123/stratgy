4764a5315704db1ed38876b0cf054285da9a740c
Ahmeed1123 <ahmedsho636@gmail.com>
2024-05-03 09:00:38 +0300

    fix styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-// the below code fragment can be found in:
+// start the change background on scoole dawn
+    let navbarBg = document.querySelector("nav.navbar");
+
+    window.onscroll = function () {
+        if (window.scrollY >= 100) {
+            navbarBg.classList.add("bg-glassy");
+        } else {
+            navbarBg.classList.remove("bg-glassy");
+        }
+    };
+// en the change background on scoole dawn
